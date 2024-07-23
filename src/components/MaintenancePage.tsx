bdeb69ae200f4145194bc2f71870d7ed37542f74
sistiansyah <36832093+sistiansyah@users.noreply.github.com>
2024-07-24 00:05:08 +0700

    Maintenance Page
    Add maintenance page

--- src/components/MaintenancePage.tsx
+++ src/components/MaintenancePage.tsx
@@ -1,9 +1,33 @@
 import "@/app/maintenance.css";
+import Image from 'next/Image';
+import gridImg from "../../public/gridnew.png";
+
 const MaintenancePage: React.FC = () => {
     return (
-        <div className="fixed inset-0 w-full h-full">
-            <div className="container w-full h-full flex flex-wrap items-center justify-center">
-                <h1>GALIH KOONTLLOOOOO</h1>
+        <div className="fixed inset-0 w-full h-full bg-white">
+            <div className="mm-wrapper">
+                <div className="mm-ov">
+                    <div style={{
+                        backgroundImage: `url(${gridImg.src})`,
+                    }}></div>
+                    <div style={{
+                        backgroundImage: `url(${gridImg.src})`,
+                    }}></div>
+                </div>
+                <div className="mm-cubes-holder">
+                    <div className="mm-cubes">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+                <div className="mm-panel">
+                    <h1>&#47;&#47; Maintenance Mode</h1>
+                    <p>
+                        This website is currently undergoing scheduled maintenance. We&apos;ll be back online shortly.
+                    </p>
+                </div>
             </div>
         </div>
     )
