8387fa73d1f8338321e27b0970f572326acc8821
Tito Oktavian <tito.oktavian@yahoo.co.id>
2024-08-10 12:34:59 +0700

    define colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,40 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          500: '#072149',
+          400: '#176EF1',
+          300: '#73A8F7',
+          200: '#B9D3FB',
+          100: '#E8EDFB',
+        },
+        secondary: {
+          500: '#0C4C3B',
+          400: '#117358',
+          300: '#A5E5D4',
+          200: '#A5E5D4',
+          100: '#E8F9F4',
+        },
+        accent: {
+          500: '#755100',
+          400: '#E3C37A',
+          300: '#FFDF99',
+          200: '#FFF2D6',
+          100: '#FFF9EB',
+        },
+        neutral: {
+          white: '#755100',
+          coolgray: {
+            900: '#22242A',
+            800: '#393D46',
+            700: '#676D7E',
+            500: '#9DA1AF',
+            400: '#D5D7DD',
+            100: '#F1F2F4',
+          },
+        },
+      },
       container: {
         center: true,
       }
