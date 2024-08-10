1859347ea2c7ee65d8a2cb5b26f49878f52b286d
sistiansyah <36832093+sistiansyah@users.noreply.github.com>
2024-08-10 21:25:53 +0700

    wip
    wip

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -6,17 +6,25 @@ import Footer from "@/components/Footer";
 import Container from "@/components/Container";
 import { usePathname } from "next/navigation";
 import PlaygroundLayout from "./PlaygroundLayout";
+import MaintenancePage from "@/components/Layouts/MaintenanceLayout";
 
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
-  const isPlayground = pathname.includes('playground')
+  const isPlayground = pathname.includes('playground');
+  const whiteListPage = ["playground"];
+  const isMaintenanceMode: boolean = String(process.env.NEXT_PUBLIC_MAINTENANCE_MODE) === '1';
+
   return (
     <Container>
-      {isPlayground ? (
+      {isPlayground && (
         <PlaygroundLayout>
           {children}
         </PlaygroundLayout>
-      ) : (
+      )}
+      {isMaintenanceMode && !isPlayground && (
+        <MaintenancePage />
+      )}
+      {!isPlayground && !isMaintenanceMode && (
         <>
           <Header />
           {children}
